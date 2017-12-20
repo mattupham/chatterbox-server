ccdbe2047340112716d9124055914e90d5ac5ff2
mattupham <mcupham@gmail.com>
2017-12-19 17:18:06 -0800

    complete get request tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -17,64 +17,94 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-//% required fs
-var fs = require('fs');
+
+var messages = [{username: 'Jono', message: 'Do my bidding!'}];
 // % added exports
 var exports = module.exports = {};
+
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
+  var statusCode;
+  var endpoints = {'/classes/messages': 'messages'};
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/JSON';
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // if (request.method == 'POST') {
-  //   console.log("POST");
-  // } else if (request.method == 'GET') {
-  //   console.log("GET");
-  // } else if (request.method == 'OPTIONS') {
-  //   console.log("OPTIONS");
-  // } else {
-  //   statusCode=404;
-  // }
 
 
+  if (request.method === 'GET') {
+    //OK
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+    //turns message into similar data structure with resulst as a key, and object as val
+    const responseBody = {};
+    responseBody.results = messages;
+    response.end(JSON.stringify(responseBody));
+  }
+
+  if (request.method === 'POST') {
+    request.on('error', (err) => {
+      console.error(err);
+      //bad request
+      response.statusCode = 400;
+      response.end();
+    });
+    response.on('error', (err) => {
+      console.error(err);
+    });
+    if (request.method === 'POST' && request.url === '/classes/messages') {
+      let body = '';
+
+      request.on('data', (chunk) => {
+        //build up stringified messages object
+        body += chunk;
+
+      });
+      response.statusCode = 201;
+      response.end();
+
+    } else {
+      //not found
+      response.statusCode = 404;
+      response.end();
+    }
+
+    //if endpoint is correct, run our post request
+    // if (endpoints[request.url] === 'messages') {
+    //   statusCode = 201;
+    //   //check to see if our endpoints match
+    //   const {method, url} = request;
+    //   //stringified message object
+    //   let body = '';
+
+    //   request.on('error', (err) => {
+    //     console.log('we hit an error');
+    //     console.error(err);
+
+    //   }).on('data', (chunk) => {
+    //     //build up stringified messages object
+    //     body += chunk;
+
+    //   });
+    // } else {
+    //   //if endpoint is incorrect, update status code to 404
+    //   statusCode = 404;
+    // }
 
 
-  response.end();
+    // request.on('end', () => {
+
+    //   response.on('error', (err) => {
+    //     console.error(err);
+    //   });
+    //   //grab incoming message, parse it to object, add to messages array
+    //   messages.push(JSON.parse(body));
+    //   response.writeHead(statusCode, headers);
+    //   const responseBody = {headers, method, url, body};
+    //   response.end(JSON.stringify(responseBody));
+    // });
+  }
+
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
